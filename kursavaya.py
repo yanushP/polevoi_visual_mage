9bc348733920f3a2be9e5a6f74cc5e46c1476f76
yanushP <yanvit97@gmail.com>
2018-04-22 16:55:01 +0300

    update kursach

--- kursavaya.py
+++ kursavaya.py
@@ -4,13 +4,18 @@ import numpy as np
 image = cv.imread('razmetka.jpg')
 image2 = cv.imread('razmetka.jpg')
 
-for i in range(9, len(image) - 9):
-    for j in range(9, len(image[i]) - 9):
-        if (image[i][j][0] < 90 and image[i][j][1] > 90 and image[i][j][2] > 90 and abs(image[i][j][2] - image[i][j][1]) < 100):
-            print ("color {0} {1}".format(i, j))
-            for x in range(-7, 8):
-                for y in range(-7, 8):
-                    image2[i + x][j + y] = [0, 0, 255]
+def check(val, max_val):
+    return val >= 0 and val < max_val
+
+
+for i in range(0, len(image)):
+    for j in range(0, len(image[i])):
+        if (image[i][j][0] < 100 and image[i][j][1] > 120 and image[i][j][2] > 120 and abs(image[i][j][2] - image[i][j][1]) < 50):
+            print ("cord {}".format((i, j)))
+            for x in range(-3, 4):
+                for y in range(-3, 4):
+                    if check(i + x, len(image)) and check(j + y, len(image[i])):
+                        image2[i + x][j + y] = [0, 0, 255]
 
 cv.imshow("Hi road", image2)
 cv.waitKey()
