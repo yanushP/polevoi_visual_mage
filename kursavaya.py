db2389b87507665ac5368a61b0004e6ee8703d7e
yanushP <yanvit97@gmail.com>
2018-05-06 20:38:12 +0300

    final version

--- kursavaya.py
+++ kursavaya.py
@@ -1,117 +1,115 @@
 import cv2 as cv
 import numpy as np
 
-#image = cv.imread('razmetra2.png')
-#image2 = cv.imread('razmetra2.png')
 
-SHOW_MODE = True
+class WhiteImage():
 
-def check(img, i, j, mi, mj):
-    return (i >= 0 and j >= 0 and i < mi and j < mj and img[i][j][0] == 255 and img[i][j][1] == 0 and img[i][j][2] == 0)
+    def __init__(self, test_image):
+        self.test_image = test_image
+        self.N = len(test_image)
+        self.M = len(test_image[0])
+        self.HOR = int(self.N * 0.45)
+        self.parts_cnt = 1
+        self.parts_n = (self.N - self.HOR) // self.parts_cnt
+        self.parts_m = self.M // self.parts_cnt
+        self._process_image()
 
+    def _create_windows(self, name, pos):
+        cv.namedWindow(name, cv.WINDOW_NORMAL)
+        cv.resizeWindow(name, 30, 30)
+        cv.moveWindow(name, pos[0], pos[1])
 
-def create_windows(name, pos):
-    cv.namedWindow(name, cv.WINDOW_NORMAL)
-    cv.resizeWindow(name, 30, 30)
-    cv.moveWindow(name, pos[0], pos[1])
+    def _process_image(self):
+        self.hsv_test_img = cv.cvtColor(self.test_image, cv.COLOR_BGR2HSV)
+        self.white_spots = np.zeros((self.N, self.M))
 
-all_img = []
+        for i in range(0, self.parts_cnt):
+            for j in range(0, self.parts_cnt):
+                l_i = self.HOR + i * self.parts_n
+                r_i = min(self.HOR + (i + 1) * self.parts_n, self.N)
+                l_j = (j * self.parts_m)
+                r_j = min((j + 1) * self.parts_m, self.M)
 
-def get_result(vali_img, test_img):
-    true_pos = false_pos = true_neg = false_neg = neg_all = pos_all = 0
+                sum_s = all_pt = 0
+                for x in range(l_i, r_i):
+                    for y in range(l_j, r_j):
+                        all_pt += 1
+                        sum_s += self.hsv_test_img[x][y][1]
+                mid_vl = max(40, (sum_s / all_pt) - 13)
 
-    for i in range(310, len(test_img)):
-        for j in range(0, len(test_img[i])):
-            real = False
-            ans = False
-            if int(vali_img[i][j][1]) - int(vali_img[i][j][0]) >= 90 and vali_img[i][j][1] > 160:
-                vali_img[i][j] = [255, 255, 255]
-                real = True
-                pos_all += 1
-            else:
-                neg_all += 1
+                for x in range(l_i, r_i):
+                    for y in range(l_j, r_j):
+                        if (self.hsv_test_img[x][y][1] <= mid_vl and self.hsv_test_img[x][y][2] > 90):
+                            self.white_spots[x][y] = 1
+                        else:
+                            self.white_spots[x][y] = 0
 
-            if test_img[i][j][0] == 255 and test_img[i][j][1] == test_img[i][j][2] and test_img[i][j][1] == 0:
-                ans = True
+        # clear image
+        self.erode_img = cv.erode(self.white_spots, np.ones((5, 5)))
+        self.dilate_img = cv.dilate(self.erode_img, np.ones((15, 15)))
+        self.result_image = cv.erode(self.dilate_img, np.ones((3, 3)))
 
-            if real and ans:
-                true_pos += 1
-            elif not real and ans:
-                false_neg += 1
+    def get_result(self, vali_img, need_print=False):
+        true_pos = false_pos = true_neg = false_neg = neg_all = pos_all = 0
+        self.vali_img = vali_img
 
-    false_pos = pos_all - true_pos
-    true_neg = neg_all - false_neg
-    print (img_num)
-    print ("Positive {0} TruePositive {1} : {2}\% FalsePositive {3}: {4}%".format(pos_all, true_pos, 100 * true_pos / pos_all, false_pos, 100 * false_pos / pos_all))
-    print ("Negative {0} TrueNegative {1} : {2}\% FalseNegative {3}: {4}%".format(neg_all, true_neg, 100 * true_neg / neg_all, false_neg, 100 * false_neg / neg_all))
-    all_img.append({"n":img_num, "p":pos_all, "n":neg_all, "tpp": 100 * true_pos / pos_all})
-    return vali_img
+        for i in range(self.HOR, self.N):
+            for j in range(0, self.M):
+                ans = True if self.result_image[i][j] == 1 else False
+                real = False
+                if int(vali_img[i][j][1]) - int(vali_img[i][j][0]) >= 90 and vali_img[i][j][1] > 160:
+                    vali_img[i][j] = [255, 255, 255]
+                    real = True
+                    pos_all += 1
+                else:
+                    neg_all += 1
 
-VALIDATE_IMG = "SYNTHIA-SEQS-01-SUMMER/GT/COLOR/Stereo_Right/Omni_F/000{}.png"
-TEST_IMG = "SYNTHIA-SEQS-01-SUMMER/RGB/Stereo_Right/Omni_F/000{}.png"
-IMG_CNT = 944
+                if real and ans:
+                    true_pos += 1
+                elif not real and ans:
+                    false_neg += 1
 
-for i in range(0, IMG_CNT):
-    i = 51
-    img_num = '0' * (3 - len(str(i))) + str(i)
-    print (TEST_IMG.format(img_num))
-    vali_img = cv.imread(VALIDATE_IMG.format(img_num))
-    test_img = cv.imread(TEST_IMG.format(img_num))
-    start_img = cv.imread(TEST_IMG.format(img_num))
-    final_img = cv.imread(TEST_IMG.format(img_num))
+        false_pos = pos_all - true_pos
+        true_neg = neg_all - false_neg
+        if need_print:
+            print ("Positive {0} TruePositive {1} : {2}\% FalsePositive {3}: {4}%".format(pos_all, true_pos, 100 * true_pos / pos_all, false_pos, 100 * false_pos / pos_all))
+            print ("Negative {0} TrueNegative {1} : {2}\% FalseNegative {3}: {4}%".format(neg_all, true_neg, 100 * true_neg / neg_all, false_neg, 100 * false_neg / neg_all))
+        return (100 * true_pos / pos_all, 100 * false_neg / neg_all)
 
-    if SHOW_MODE:
-        create_windows("before_vali_img", (600, 600))
-        create_windows("before_test_img", (900, 600))
-        create_windows("test_img_before_erod", (300, 300))
-        create_windows("vali_img", (600, 0))
-        create_windows("test_img", (200, 0))
-        cv.imshow("before_test_img", test_img)
-        cv.imshow("before_vali_img", vali_img)
-        for i in range(300, 310):
-            for j in range(0, len(test_img[i])):
-                test_img[i][j] = [0, 255, 255]
-                vali_img[i][j] = [0, 255, 255]
+    def show_res_images(self):
+        self._create_windows("default_image", (0, 0))
+        self._create_windows("validation_image", (100, 100))
+        self._create_windows("final_image", (300, 300))
+        cv.imshow("default_image", self.test_image)
 
-    hsv_test_img = cv.cvtColor(test_img, cv.COLOR_BGR2HSV)
+        for i in range(self.HOR - 10, self.HOR):
+            for j in range(0, self.M):
+                self.test_image[i][j] = [0, 255, 255]
 
-    sum_s = all_pt = 0
-    for i in range(310, len(test_img)):
-        for j in range(0, len(test_img[i])):
-            all_pt += 1
-            sum_s += hsv_test_img[i][j][1]
+        for i in range(self.HOR, self.N):
+            for j in range(0, self.M):
+                self.test_image[i][j] = [255, 255, 255] if self.result_image[i][j] == 1 else self.test_image[i][j]
 
-    s_md = (sum_s / all_pt)
-    print (s_md)
-    mid_vl = 30 + (65 - s_md / 2)
-    print (mid_vl)
-
-    for i in range(310, len(test_img)):
-        for j in range(0, len(test_img[i])):
-            wt_cl = 110
-            if (hsv_test_img[i][j][1] <= mid_vl and hsv_test_img[i][j][2] > 40
-                and test_img[i][j][0] > wt_cl and test_img[i][j][1] > wt_cl and test_img[i][j][1] > wt_cl):
-                test_img[i][j] = [255, 0, 0]
-
-    erode_img = cv.erode(test_img, np.ones((5, 5)))
-    hsv_test_img = cv.cvtColor(erode_img, cv.COLOR_BGR2HSV)
-    if SHOW_MODE:
-        cv.imshow("test_img_before_erod", test_img)
-
-    for i in range(310, len(start_img)):
-        for j in range(0, len(start_img[i])):
-            if hsv_test_img[i][j][0] > 110 and hsv_test_img[i][j][0] < 130:
-                start_img[i][j] = [255, 0, 0]
-    vali_img = get_result(vali_img, start_img)
-
-    if SHOW_MODE:
-        cv.imshow("vali_img", erode_img)
-        cv.imshow("test_img", start_img)
-        cv.imshow("vali_img1", vali_img)
+        cv.imshow("validation_image", self.vali_img)
+        cv.imshow("final_image", self.test_image)
 
         if cv.waitKey():
-            break
-        cv.destroyAllWindows()
+            cv.destroyAllWindows()
 
-all_img.sort(key=lambda x: x['tpp'])
-print (all_img)
+
+def main():
+    VALIDATE_IMG = "SYNTHIA-SEQS-01-SUMMER/GT/COLOR/Stereo_Right/Omni_F/000{}.png"
+    TEST_IMG = "SYNTHIA-SEQS-01-SUMMER/RGB/Stereo_Right/Omni_F/000{}.png"
+    IMG_CNT = 944
+
+    for i in range(0, IMG_CNT):
+        img_num = '0' * (3 - len(str(i))) + str(i)
+        print (TEST_IMG.format(img_num))
+        vali_img = cv.imread(VALIDATE_IMG.format(img_num))
+        test_img = cv.imread(TEST_IMG.format(img_num))
+        mat = WhiteImage(test_img)
+        mat.get_result(vali_img, True)
+        #mat.show_res_images()
+
+if __name__ == "__main__":
+    main()
